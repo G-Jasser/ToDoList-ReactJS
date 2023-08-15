4d2b4a525dd236629136167b737e6c98c2c2f075
Jasser Gharb <123377848+G-Jasser@users.noreply.github.com>
2023-08-15 16:42:52 +0100

    Add filter tasks by not done/all/done

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -5,6 +5,12 @@ import {useSelector } from 'react-redux'
 
 const TaskList = () => {
     const todoList = useSelector((state) => state.todoLister.tasklist)
+    
+    const [filterSetting, setFilterSetting] = useState(0);
+
+    const handleChange = (radioInput) => {
+    setFilterSetting(+radioInput.currentTarget.value);
+    };
 
     return (
         <div className='TODO-container'>
@@ -12,11 +18,54 @@ const TaskList = () => {
             {
                 todoList.length>0
                 ?
+                <>
+                <ul className='TODO-filter-container'>
+                    <h2>Filter: </h2>
+                    <li>
+                        <label htmlFor='radio1'>Not Done</label>
+                        <input
+                            type='radio'
+                            id='radio1'
+                            value='-1'
+                            onChange={handleChange}
+                            checked={filterSetting === -1}
+                        />
+                    </li>
+                    <li>
+                        <label htmlFor='radio2'>All</label>
+                        <input
+                            type='radio'
+                            id='radio2'
+                            value='0'
+                            onChange={handleChange}
+                            checked={filterSetting === 0}
+                        />
+                    </li>
+                    <li>
+                        <label htmlFor='radio3'>Done</label>
+                        <input
+                            type='radio'
+                            id='radio3'
+                            value='1'
+                            onChange={handleChange}
+                            checked={filterSetting === 1}
+                        />
+                    </li>
+                </ul>
+
                 <div className='TODO-card-container'>
                     {
+                        filterSetting === 1?
+                        todoList.filter((task)=>task.isDone).map((task) => <TaskCard task={task} key={task.id} />)
+                        :
+                        filterSetting === -1?
+                        todoList.filter((task)=>!task.isDone).map((task) => <TaskCard task={task} key={task.id} />)
+                        :
                         todoList.map((task) => <TaskCard task={task} key={task.id} />)
+
                     }
                 </div>
+                </>
                 :
                 <h1>
                     ADD TASKS
